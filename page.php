18aba83e789de48debd2edb7a14d5794615869fa
tamimhasan19702 <tamimhasancu@gmail.com>
2023-10-07 02:03:56 +0600

    new theme support added to the theme

--- page.php
+++ page.php
@@ -11,7 +11,7 @@
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
-            <p>Learn how the school of your dreams got started.</p>
+            <p>Don't Forget to Replace me later</p>
         </div>
     </div>
 </div>
@@ -24,25 +24,16 @@
         </p>
     </div>
 
-    <div class="page-links">
+    <!-- <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
             <li class="current_page_item"><a href="#">Our History</a></li>
             <li><a href="#">Our Goals</a></li>
         </ul>
-    </div>
+    </div> -->
 
     <div class="generic-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid
-            possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis,
-            consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias
-            atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad
-            quod sed.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid
-            possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis,
-            consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias
-            atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad
-            quod sed.</p>
+        <?php the_content(); ?>
     </div>
 </div>
 
