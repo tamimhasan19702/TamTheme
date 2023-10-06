50e094f18e780aa14cc5b11283256e9562487bf0
tamimhasan19702 <tamimhasancu@gmail.com>
2023-10-06 23:48:11 +0600

    update index.php and page.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+get_header();
 while(have_posts()){
     the_post(); ?>
 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -6,4 +7,5 @@ while(have_posts()){
 <hr>
 <?php 
 }
+get_footer();
 ?>
