ca40b9b036d6a75976a9208656a265095595283b
tamimhasan19702 <tamimhasancu@gmail.com>
2023-10-07 01:37:09 +0600

    added jquery slider to my theme

--- functions.php
+++ functions.php
@@ -1,10 +1,13 @@
 <?php 
 
 function university_files(){
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'),['jquery'] , '1.0',true);
     wp_enqueue_style('university_main_styles',get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_styles',get_theme_file_uri('/build/index.css'));
+    wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 }
 
-add_action('wp_enqueue_scripts', 'university files');
+add_action('wp_enqueue_scripts', 'university_files');
 
 ?>
