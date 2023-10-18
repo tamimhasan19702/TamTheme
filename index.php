0c546e787b2b5d95e0723f7fba9ecf6be538c274
tamimhasan19702 <tamimhasancu@gmail.com>
2023-10-18 18:32:35 +0600

    blog page updates

--- index.php
+++ index.php
@@ -21,15 +21,26 @@ get_header();
     while (have_posts()) {
         the_post();
         ?>
-        <div class="post-item">
-            <h2><a href="<php the_permalink(); ?>">
-                    <?php
+    <div class="post-item">
+        <h2 class="headline headline--medium headline--post-title"><a href="<php the_permalink(); ?>">
+                <?php
                     the_title();
-                    the_excerpt();
                     ?>
-                </a></h2>
+            </a></h2>
+
+        <div class="metabox">
+            <p>Posted by <?php the_author_posts_link(); ?> on 10.18.2023 </p>
         </div>
-        <?php
+
+        <div class="generic-content">
+            <?php the_excerpt(); ?>
+            <p><a href="<?php the_permalink(); ?>" class="btn btn--blue">
+                    Continue reading &raquo;
+                </a></p>
+        </div>
+
+    </div>
+    <?php
     }
     ;
     ?>
