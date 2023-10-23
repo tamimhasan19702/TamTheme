634ce5c5cc457edb0e9d51d22f1fa251788be224
tamimhasan19702 <tamimhasancu@gmail.com>
2023-10-24 01:09:21 +0600

    single event and archive page created

--- front-page.php
+++ front-page.php
@@ -33,23 +33,23 @@ get_header();
                 $homapageEvents->the_post();
                 ?>
 
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Jan</span>
-                    <span class="event-summary__day">20</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">
-                            <?php the_title() ?>
-                        </a>
-                    </h5>
-                    <p>
-                        <?php echo wp_trim_words(get_the_content(), 18) ?>
-                        <a href="#" class="nu gray">Read more</a>
-                    </p>
+                <div class="event-summary">
+                    <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                        <span class="event-summary__month">Jan</span>
+                        <span class="event-summary__day">20</span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>">
+                                <?php the_title() ?>
+                            </a>
+                        </h5>
+                        <p>
+                            <?php echo wp_trim_words(get_the_content(), 18) ?>
+                            <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a>
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <?php
+                <?php
             }
 
             ?>
@@ -75,27 +75,27 @@ get_header();
                 $homapagePosts->the_post();
                 ?>
 
-            <div class="event-summary">
-                <a class=" event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-                    <span class="event-summary__month">
-                        <?php the_time('M') ?>
-                    </span>
-                    <span class="event-summary__day">
-                        <?php the_time('d') ?>
-                    </span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>">
-                            <?php the_title() ?>
-                        </a></h5>
-                    <p>
-                        <?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink(); ?>"
-                            class="nu gray">Read more</a>
-                    </p>
+                <div class="event-summary">
+                    <a class=" event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                        <span class="event-summary__month">
+                            <?php the_time('M') ?>
+                        </span>
+                        <span class="event-summary__day">
+                            <?php the_time('d') ?>
+                        </span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>">
+                                <?php the_title() ?>
+                            </a></h5>
+                        <p>
+                            <?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink(); ?>"
+                                class="nu gray">Read more</a>
+                        </p>
+                    </div>
                 </div>
-            </div>
 
-            <?php
+                <?php
             }
             wp_reset_postdata();
             ?>
