279855a2b5553f91e17ba06c9e019400c42756ff
tamimhasan19702 <tamimhasancu@gmail.com>
2023-10-28 15:44:20 +0600

    adding relationsl custom field to programs and event

--- archive-program.php
+++ archive-program.php
@@ -18,48 +18,26 @@ get_header();
 </div>
 
 <div class="container container--narrow page-section">
-    <?php
 
-    while (have_posts()) {
-        the_post();
-        ?>
-        <div class="event-summary">
-            <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month">
-                    <?php
-                    $eventDate = new DateTime(get_field('event_date'));
-                    echo $eventDate->format('M');
-                    ?>
-                </span>
-                <span class="event-summary__day">
-                    <?php
-                    $eventDate = new DateTime(get_field('event_date'));
-                    echo $eventDate->format('d');
-                    ?>
-                </span>
-            </a>
-            <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>">
-                        <?php the_title() ?>
-                    </a>
-                </h5>
-                <p>
-                    <?php echo wp_trim_words(get_the_content(), 30) ?>
-                    <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a>
-                </p>
-            </div>
-        </div>
+    <ul class="link-list min-list">
+
         <?php
-    }
 
-    echo paginate_links();
+        while (have_posts()) {
+            the_post();
+            ?>
+            <li><a href="<?php the_permalink(); ?>">
+                    <?php the_title() ?>
+                </a>
+            </li>
+            <?php
+        }
 
-    ?>
+        echo paginate_links();
 
-    <hr class="section-break">
+        ?>
+    </ul>
 
-    <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events'); ?>">Check out our past events
-            archive</a></p>
 
 </div>
 <?php
--- functions.php
+++ functions.php
@@ -25,6 +25,12 @@ function university_features()
 
 function University_adjust_query($query)
 {
+    if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', '-1');
+    }
+
     if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
         $today = date('Ymd');
         $query->set('meta_key', 'event_date');
