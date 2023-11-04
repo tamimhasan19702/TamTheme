46a5cd15971485af3dc0c75bcc330c9bf49565fe
tamimhasan19702 <tamimhasancu@gmail.com>
2023-11-04 23:04:41 +0600

    theme files updates

--- single-event.php
+++ single-event.php
@@ -45,14 +45,20 @@ while (have_posts()) {
             echo '<ul class="link-list min-list">';
             foreach ($relatedPrograms as $program) {
                 ?>
-                <li><a href="<?php echo get_the_permalink($program); ?>">
+
+                <li>
+                    <a href="<?php echo get_the_permalink($program); ?>">
                         <?php echo get_the_title($program); ?>
-                    </a></li>
+                    </a>
+                </li>
+
                 <?php
             }
             echo '</ul>';
 
         }
+
+
 
 
 
