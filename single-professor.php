4017527ee29027f6f3fe2aabe463ff0f89da29ff
tamimhasan19702 <tamimhasancu@gmail.com>
2023-11-04 23:49:29 +0600

    professor post type added to the programs post type

--- single-professor.php
+++ single-professor.php
@@ -19,16 +19,6 @@ while (have_posts()) {
 
 <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i
-                    class="fa fa-home" aria-hidden="true"></i>
-                Events Home
-            </a> <span class="metabox__main">
-                <?php the_title(); ?>
-            </span>
-        </p>
-    </div>
 
     <div class="generic-content">
         <?php the_content() ?>
@@ -43,7 +33,7 @@ while (have_posts()) {
             echo '<hr class="section-break">';
             echo '<h2 class="headline headline--medium">Related Programs(s)</h2>';
             echo '<ul class="link-list min-list">';
-            
+
             foreach ($relatedPrograms as $program) {
                 ?>
 
@@ -58,10 +48,6 @@ while (have_posts()) {
             echo '</ul>';
 
         }
-
-
-
-
 
         ?>
 
