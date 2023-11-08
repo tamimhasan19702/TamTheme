a1ec67a155b7257225bb3d2b74c2963ccba47fe9
tamimhasan19702 <tamimhasancu@gmail.com>
2023-11-09 00:12:00 +0600

    professor post type updates

--- functions.php
+++ functions.php
@@ -24,6 +24,7 @@ function university_features()
     add_theme_support('post-thumbnails');
     add_image_size('professorLandscape', 400, 260, true);
     add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
 }
 
 function University_adjust_query($query)
