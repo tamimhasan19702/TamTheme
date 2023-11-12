6ab46e903687cc466de3b308b24a7b912bdc9949
tamimhasan19702 <tamimhasancu@gmail.com>
2023-11-12 19:19:09 +0600

    pageBanner function updates

--- functions.php
+++ functions.php
@@ -2,26 +2,38 @@
 
 // test commit
 
-function pageBanner()
+function pageBanner($args = Null)
 {
+    if (!$args['title']) {
+        $args['title'] = get_the_title();
+    }
+    if (!$args['subtitle']) {
+        $args['subtitle'] = the_field('page_banner_subtitle');
+    }
+    if ($args['photo']) {
+        if (get_field('page_banner_background')) {
+            $args['photo'] = get_field('page_banner_background')['sizes']['pageBanner'];
+        } else {
+            $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+        }
+    }
     ?>
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php
-        $pageBannerImage = get_field('page_banner_background');
-        echo $pageBannerImage['url']
-            ?>)"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            <?php the_title(); ?>
-        </h1>
-        <div class="page-banner__intro">
-            <p>
-                <?php the_field('page_banner_subtitle'); ?>
-            </p>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php
+        echo $args['photo'];
+        ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title">
+                <?php echo $args['title'] ?>
+            </h1>
+            <div class="page-banner__intro">
+                <p>
+                    <?php echo $args['subtitle'] ?>
+                </p>
+            </div>
         </div>
     </div>
-</div>
-<?php
+    <?php
 }
 
 function university_files()
