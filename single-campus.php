d34c6acf9ab7cbbc83f2f4e8170f544581f71ee5
tamimhasan19702 <tamimhasancu@gmail.com>
2023-12-05 18:49:22 +0600

    relationship converged successfully with the program and campus post type

--- single-campus.php
+++ single-campus.php
@@ -6,47 +6,47 @@ while (have_posts()) {
     pageBanner();
     ?>
 
-    <div class="container container--narrow page-section">
+<div class="container container--narrow page-section">
 
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('campus'); ?>"><i
-                        class="fa fa-home" aria-hidden="true"></i>
-                    All Campuses
-                </a> <span class="metabox__main">
-                    <?php the_title(); ?>
-                </span>
-            </p>
+    <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('campus'); ?>"><i
+                    class="fa fa-home" aria-hidden="true"></i>
+                All Campuses
+            </a> <span class="metabox__main">
+                <?php the_title(); ?>
+            </span>
+        </p>
+    </div>
+
+    <div class="generic-content">
+
+        <?php the_content() ?>
+    </div>
+
+    <!-- campus map type -->
+
+    <div class="acf-map">
+        <div>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d35754.661314876335!2d91.79875245749197!3d22.472668465507628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd6fe9a3a4473%3A0x7836276aef538552!2sUniversity%20of%20Chittagong!5e1!3m2!1sen!2sbd!4v1701613237739!5m2!1sen!2sbd"
+                width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+
         </div>
-
-        <div class="generic-content">
-
-            <?php the_content() ?>
-        </div>
-
-        <!-- campus map type -->
-
-        <div class="acf-map">
-            <div>
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d35754.661314876335!2d91.79875245749197!3d22.472668465507628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd6fe9a3a4473%3A0x7836276aef538552!2sUniversity%20of%20Chittagong!5e1!3m2!1sen!2sbd!4v1701613237739!5m2!1sen!2sbd"
-                    width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-            </div>
-            </a>
-        </div>
+        </a>
+    </div>
 
 
-        <?php
-        $relatedProfessors = new WP_Query([
+    <?php
+        $relatedPrograms = new WP_Query([
             'posts_per_page' => -1,
-            'post_type' => 'professor',
+            'post_type' => 'program',
             'orderby' => 'title',
             'order' => 'ASC',
             'meta_query' => [
                 array(
-                    'key' => 'related_programs',
+                    'key' => 'related_campus',
                     'compare' => 'LIKE',
                     'value' => '"' . get_the_ID() . '"'
                 )
@@ -54,28 +54,21 @@ while (have_posts()) {
         ]);
 
 
-        if ($relatedProfessors->have_posts()) {
+        if ($relatedPrograms->have_posts()) {
 
             echo '<hr class="section-break">';
-            echo '<h2 class="headline headline--medium">  ' . get_the_title() . ' Professors</h2>';
+            echo '<h2 class="headline headline--medium">  Programs Available at this campus</h2>';
 
-            echo '<ul class="professor-cards">';
-            while ($relatedProfessors->have_posts()) {
-                $relatedProfessors->the_post();
+            echo '<ul class="min-list link-list">';
+            while ($relatedPrograms->have_posts()) {
+                $relatedPrograms->the_post();
                 ?>
-                <li class="professor-card__list-item">
-
-                    <a class="professor-card" href="<?php the_permalink(); ?>">
-
-                        <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
-
-                        <span class="professor-card__name">
-                            <?php the_title(); ?>
-                        </span>
-
-                    </a>
-                </li>
-                <?php
+    <li>
+        <a href="<?php the_permalink(); ?>">
+            <?php the_title();?>
+        </a>
+    </li>
+    <?php
             }
             echo '</ul>';
         }
@@ -123,9 +116,9 @@ while (have_posts()) {
 
 
 
-    </div>
+</div>
 
-    <?php
+<?php
 }
 
 get_footer();
