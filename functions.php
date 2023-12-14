28ddf3b5c3f51b4be389dcded15d74d04d5a43b6
tamimhasan19702 <tamimhasancu@gmail.com>
2023-12-14 23:23:36 +0600

    updates on theme files

--- functions.php
+++ functions.php
@@ -8,32 +8,32 @@ function pageBanner($args = NULL)
     $args['photo'] = isset($args['photo']) ? $args['photo'] : (get_field('page_banner_background') ? get_field('page_banner_background')['sizes']['pageBanner'] : get_theme_file_uri('/images/ocean.jpg'));
     ?>
 
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(
         <?php echo $args['photo'] ?>
     )"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            <?php echo $args['title'] ?>
-        </h1>
-        <div class="page-banner__intro">
-            <p>
-                <?php echo $args['subtitle'] ?>
-            </p>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title">
+                <?php echo $args['title'] ?>
+            </h1>
+            <div class="page-banner__intro">
+                <p>
+                    <?php echo $args['subtitle'] ?>
+                </p>
+            </div>
         </div>
     </div>
-</div>
-<?php
+    <?php
 
 
 }
 
 function university_files()
 {
-    wp_enqueue_script('main-university-js', get_theme_file_uri('./build/index.js'), ['jquery'], '1.0', true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/src/index.js'), ['jquery'], '1.0', true);
     wp_enqueue_script('googleMap', '', NULL, '1.0', true);
-    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
-    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/css/style.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/css/style.css.map'));
     wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 }
--- src/modules/Search.js
+++ src/modules/Search.js
@@ -3,28 +3,28 @@
 class Search {
   // discribe and create/initialize our object
   constructor() {
-    // this.openButton = $(".js-search-trigger");
-    // this.closeButton = $(".search-overlay__close");
-    // this.searchOverlay = $(".search-overlay");
-    // this.events();
+    this.openButton = $(".js-search-trigger");
+    this.closeButton = $(".search-overlay__close");
+    this.searchOverlay = $(".search-overlay");
+    this.events();
     console.log("search");
   }
 
-  // // events
-  // events() {
-  //   this.openButton.on("click", this.openOverlay.bind(this));
-  //   this.closeButton.on("click", this.closeOverlay.bind(this));
-  // }
+  // events
+  events() {
+    this.openButton.on("click", this.openOverlay.bind(this));
+    this.closeButton.on("click", this.closeOverlay.bind(this));
+  }
 
-  // //   methods (function, action..)
-  // openOverlay() {
-  //   this.searchOverlay.addClass("search-overlay--active");
-  //   this.body.addClass("body-no-scroll");
-  // }
-  // closeOverlay() {
-  //   this.searchOverlay.removeClass("search-overlay--active");
-  //   this.body.removeClass("body-no-scroll");
-  // }
+  //   methods (function, action..)
+  openOverlay() {
+    this.searchOverlay.addClass("search-overlay--active");
+    this.body.addClass("body-no-scroll");
+  }
+  closeOverlay() {
+    this.searchOverlay.removeClass("search-overlay--active");
+    this.body.removeClass("body-no-scroll");
+  }
 }
 
 export default Search;
