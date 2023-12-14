69335e779202afbe81e9cb4590af0118adad99f8
tamimhasan19702 <tamimhasancu@gmail.com>
2023-12-15 01:41:41 +0600

    search method overlays updates

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -8,23 +8,36 @@ class Search {
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
     this.events();
-    console.log("search");
+    this.isOverlayOpen = false;
   }
 
   // events
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
+    $(document).on("keyup", this.keyPressDispatcher.bind(this));
   }
 
   //   methods (function, action..)
+  keyPressDispatcher(e) {
+    if (e.keyCode == 83 && !this.isOverlayOpen) {
+      this.openOverlay();
+    }
+
+    if (e.keyCode == 27 && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
+  }
+
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
-    this.body.addClass("body-no-scroll");
+    $("body").addClass("body-no-scroll");
+    this.isOverlayOpen = true;
   }
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active");
-    this.body.removeClass("body-no-scroll");
+    $("body").removeClass("body-no-scroll");
+    this.isOverlayOpen = false;
   }
 }
 
