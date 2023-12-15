2653c2ce3a119b2a27319ff0df18902dfc03c617
tamimhasan19702 <tamimhasancu@gmail.com>
2023-12-15 20:54:11 +0600

    search overlay update with search query timer and icon style fixed

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -7,18 +7,27 @@ class Search {
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
+    this.searchField = $("#search-term");
     this.events();
     this.isOverlayOpen = false;
+    this.typingTimer;
   }
 
   // events
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
-    $(document).on("keyup", this.keyPressDispatcher.bind(this));
+    $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keydown", this.typingLogic.bind(this));
   }
 
   //   methods (function, action..)
+  typingLogic() {
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout(() => {
+      console.log("this is timeout setup");
+    }, 2000);
+  }
   keyPressDispatcher(e) {
     if (e.keyCode == 83 && !this.isOverlayOpen) {
       this.openOverlay();
