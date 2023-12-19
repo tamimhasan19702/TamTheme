4fd8fb5c00741aa211387b859104420994a8ab47
tamimhasan19702 <tamimhasancu@gmail.com>
2023-12-19 21:01:54 +0600

    refactored search code a bit

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -4,6 +4,7 @@ import $ from "jquery";
 class Search {
   // discribe and create/initialize our object
   constructor() {
+    this.searchOverlayHtml();
     this.resultsDiv = $("#search-overlay__results");
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
@@ -92,6 +93,24 @@ class Search {
     this.searchOverlay.removeClass("search-overlay--active");
     $("body").removeClass("body-no-scroll");
     this.isOverlayOpen = false;
+  }
+
+  searchOverlayHtml() {
+    $("body").append(`
+    <div class="search-overlay ">
+    <div class="search-overlay__top">
+        <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+        </div>
+    </div>
+
+    <div class="container">
+        <div id="search-overlay__results"></div>
+    </div>
+</div>
+    `);
   }
 }
 
