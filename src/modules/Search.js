7fcf0281085f3c600ce0947d0970860ead8fe260
tamimhasan19702 <tamimhasancu@gmail.com>
2023-12-20 21:53:45 +0600

    sync async applied to my custom search

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -45,27 +45,38 @@ class Search {
   }
 
   getResults() {
-    $.getJSON(
-      universityData.root_url +
-        "/wp-json/wp/v2/posts?search=" +
-        this.searchField.val(),
-      (posts) => {
+    $.when(
+      $.getJSON(
+        universityData.root_url +
+          "/wp-json/wp/v2/posts?search=" +
+          this.searchField.val()
+      ),
+      $.getJSON(
+        universityData.root_url +
+          "/wp-json/wp/v2/pages?search=" +
+          this.searchField.val()
+      )
+    ).then(
+      (posts, pages) => {
+        var combinedResults = posts[0].concat(pages[0]);
         this.resultsDiv.html(`
-        <h1 class="search-overlay__section-title">General Information</h1>
-        ${
-          posts.length
-            ? `<ul class="link-list min-list">`
-            : `<p>No results Found for this Keyword`
-        }
-        ${posts
-          .map(
-            (item) =>
-              `<li><a href="${item.link}">${item.title.rendered}</a></li>`
-          )
-          .join("")}
-        ${posts.length ? `</ul>` : `</p>`}
-        `);
+      <h1 class="search-overlay__section-title">General Information</h1>
+      ${
+        combinedResults.length
+          ? `<ul class="link-list min-list">`
+          : `<p>No results Found for this Keyword`
+      }
+      ${combinedResults
+        .map(
+          (item) => `<li><a href="${item.link}">${item.title.rendered}</a></li>`
+        )
+        .join("")}
+      ${combinedResults.length ? `</ul>` : `</p>`}
+      `);
         this.isSpinnerVisible = false;
+      },
+      () => {
+        this.resultsDiv.html(`<h1 >Unexpected error; Please try again</h1>`);
       }
     );
   }
@@ -87,6 +98,7 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    this.searchField.val("");
     setTimeout(() => this.searchField.focus(), 301);
     this.isOverlayOpen = true;
   }
