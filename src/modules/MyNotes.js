4a0dd15558db45eed80c49dbacc930ff0bbca893
tamimhasan19702 <tamimhasancu@gmail.com>
2024-01-28 22:39:05 +0600

    deleting my notes frontend updates

--- src/modules/MyNotes.js
+++ src/modules/MyNotes.js
@@ -6,16 +6,31 @@ class MyNotes {
   }
   events() {
     $(".delete-note").on("click", this.deleteNote);
+    $(".edit-note").on("click", this.editNote);
   }
 
-  deleteNote() {
+  editNote(e) {
+    const thisNote = $(e.target).parents("li");
+    thisNote
+      .find(".note-title-field, .note-body-field")
+      .removeAttr("readonly")
+      .addClass("note-active-field");
+
+    thisNote.find(".update-note").addClass("update-note--visible");
+  }
+
+  deleteNote(e) {
+    const thisNote = $(e.target).parents("li");
+
     $.ajax({
       beforeSend: (xhr) => {
         xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
       },
-      url: universityData.root_url + "/wp-json/wp/v2/note/170",
+      url:
+        universityData.root_url + "/wp-json/wp/v2/note/" + thisNote.data("id"),
       type: "DELETE",
       success: (response) => {
+        thisNote.slideUp();
         console.log(response);
         console.log("congrets");
       },
