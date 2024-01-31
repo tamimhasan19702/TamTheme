66e706f164153d15ddce4e21a938bdfd4081973b
tamimhasan19702 <tamimhasancu@gmail.com>
2024-01-31 22:09:40 +0600

    working on creating new Notes on the frontend

--- src/modules/MyNotes.js
+++ src/modules/MyNotes.js
@@ -8,6 +8,7 @@ class MyNotes {
     $(".delete-note").on("click", this.deleteNote);
     $(".edit-note").on("click", this.editNote.bind(this));
     $(".update-note").on("click", this.updateNote.bind(this));
+    $(".submit-note").on("click", this.createNote.bind(this));
   }
 
   editNote(e) {
@@ -87,6 +88,44 @@ class MyNotes {
         this.makeNoteReadOnly(thisNote);
         console.log(response);
         console.log("congrets");
+      },
+      error: (response) => {
+        console.log("error");
+        console.log(response);
+      },
+    });
+  }
+
+  createNote(e) {
+    const ourNewPost = {
+      " title": thisNote.find(".new-note-title").val(),
+      content: thisNote.find(".new-note-body").val(),
+      status: "publish",
+    };
+
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
+      },
+      url: universityData.root_url + "/wp-json/wp/v2/note/",
+      type: "POST",
+      data: ourNewPost,
+      success: (response) => {
+        $(".new-note-title, .new-note-body").val("");
+        $(`
+        <li data-id="${response.id}">
+        <input readonly class="note-title-field" value="${response.title.raw}">
+        <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
+        <span class="delete-note"><i class="fa fa-trash" aria-hidden="true"></i>Delete</span>
+        <textarea readonly
+            class="note-body-field">${response.content.raw}</textarea>
+        <span class="update-note btn btn--blue btn-small"><i class="fa fa-arrow-right"
+                aria-hidden="true"></i>Save</span>
+    </li>
+        `)
+          .prependTo("#my-notes")
+          .hide()
+          .slideDown();
       },
       error: (response) => {
         console.log("error");
